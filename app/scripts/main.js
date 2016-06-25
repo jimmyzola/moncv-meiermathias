b3b87295009216652179afb3d26f21443b2fe19d
meiermathias <mathiasmeier.11@gmail.com>
2016-06-25 14:48:45 +0200

    Change css div and add new music elevator

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   'use strict';
-  $('a').smoothScroll({speed:1000});
+  $('a').smoothScroll({speed:3000});
 });
 
 // Elevator script included on the page, already.
@@ -8,32 +8,45 @@ window.onload = function() {
   var elevator = new Elevator({
     element: document.querySelector('.elevator-button'),
     targetElement: document.querySelector('#elevator-target'),
-    mainAudio: '/images/panda.mp3',
-    endAudio: '/images/panda.mp3'
+    mainAudio: 'http://tholman.com/elevator.js/music/elevator.mp3',
+    endAudio: 'http://tholman.com/elevator.js/music/ding.mp3'
   });
 }
 
 $('div.progress').each(function(i, obj){
-
   var label = $(obj).find('div').text();
   var value = $(obj).find('div').attr('aria-valuenow');
   var replaceProgessDonut = $('<canvas width="400" height="400"></canvas>');
   $(obj).replaceWith(replaceProgessDonut);
+
 var myChart = new Chart(replaceProgessDonut, {
       type: 'doughnut',
       data: {
-          labels: [label],
+          labels: [label+value+'%'],
           datasets: [{
               data: [value, 100-value],
               backgroundColor: [
-                  'rgba(153, 102, 255, 0.2)'
+                  'rgba(39, 43, 48, 0.2)','#8a9196'
               ],
               borderColor: [
-                  'rgba(153, 102, 255, 1)'
+                  'rgba(39, 43, 48, 1)','#8a9196'
               ],
+            hoverBackgroundColor: [
+              'rgba(39, 43, 48, 0.2)',
+              '#8a9196'
+            ],
+            hoverBorderColor: [
+              'rgba(39, 43, 48, 0.2)',
+              '#8a9196'
+            ],
               borderWidth: 1
           }]
-      }
+      },
+  options: {
+    tooltips:{enabled:false} // désactivation du tooltips pour éviter de remarquer la zone trensparante
+  }
   });
 
 });
+
+
